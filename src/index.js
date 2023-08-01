3f9316ea84c081c363d7e909261ed9ef0d0d9298
GurmanKD <gurmankaur0603@gmail.com>
2023-08-01 21:46:59 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,44 +1,115 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
+import './index.css';
+
+// function Greeting() {
+//   return (
+//     <React.Fragment>
+//       <h2>My first component</h2>
+//       <ul>
+//         <li>
+//           <a href='#'>hi</a>
+//         </li>
+//       </ul>
+//     </React.Fragment>
+//   );
+// }
+
+// const Greeting2 = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h2", {}, "hello world")
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<Greeting></Greeting>);
+// root.render(<Greeting2></Greeting2>);
+
+// //Nesting
+// function Greeting3() {
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//   );
+// }
+
+// const Person = () => <h2>Gurman Kaur</h2>;
+// const Message = () => {
+//   return <p>this is my message</p>;
+// };
+// root.render(<Greeting3></Greeting3>);
+
+const author = 'Jordan Moore';
+const title = 'Interesting Facts For Curious Minds';
+const img = './images/book-1.jpg';
+
+// // parameters
+// const someFunc = (param1, param2) => {
+//   console.log(param1, param2);
+// }; 
+// // arguments
+// someFunc("job", "developer");
+
+
+function BookList() {
   return (
-    <React.Fragment>
-      <h2>My first component</h2>
-      <ul>
-        <li>
-          <a href='#'>hi</a>
-        </li>
-      </ul>
-    </React.Fragment>
-  );
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
 }
 
-const Greeting2 = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "hello world")
-  );
+const Book = () => {
+
+    return (
+      <article className='book'>
+        <img
+          src={img}
+          alt={title}
+        />
+        <h2>{title}</h2>
+        <h4>{author.toUpperCase()}</h4>
+        {/* <p>{let x=6}</p>  */}
+        {/* only return statements are allowed inside{} */}
+      </article>
+    )
 };
+
+// const Image = () => (
+//   <img src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
+//   alt="Interesting Facts for Curious Minds"/>
+// )
+
+// const Image = () => (
+//   <img src='./images/book-1.jpg' alt='Interesting Facts For Curious Minds' />
+// )
+//whatever assets we place in public - instantly available
+
+// const Title = () => {
+//   return <h2>Interesting Facts For Curious Minds</h2>
+// };
+// const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.5rem'}}>Jordan Moore</h4>;
+
+//// WAY 2
+// const Author1 = ()=>{
+//   const inlineHeadingStyles = {
+//     color: '#617d98',
+//     fontSize: '0.75rem',
+//     marginTop: '0.5rem',
+//   };
+//   return<h4 styles={inlineHeadingStyles}>Jordan Moore</h4>;
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting></Greeting>);
-root.render(<Greeting2></Greeting2>);
+root.render(<BookList />);
 
-//Nesting
-function Greeting3() {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  );
-}
-
-const Person = () => <h2>Gurman Kaur</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
-};
-root.render(<Greeting3></Greeting3>);
